19eb665a6e8eace3024a8e588ef821be79daa4c4
HardMachinesCorporation <thejordach@gmail.com>
2025-04-06 06:36:20 +0100

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,9 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss','@nuxtjs/google-fonts'],
+  nitro: {
+    preset: 'node-server' // <-- très important pour Coolify
+  },
   googleFonts: {
     families: {
       Inter: [100, 300, 400, 700, 900]
